cf57b30457faacd57e28a7c0264d827eb5bc59a3
robson1872 <48591084+robson1872@users.noreply.github.com>
2020-11-28 00:01:57 -0300

    Update f.cpp

--- f.cpp
+++ f.cpp
@@ -1,8 +1,31 @@
-#include<iostream>
+#include <iostream>
+#define ll long long 
 using namespace std;
-long long a,b,s,t,m;
+ll lol(ll a, ll b, ll &x, ll &y) {
+    if(b == 0) {
+       x = 1;
+       y = 0;
+       return a;
+    }
+    ll x1, y1, mdc = lol(b, a % b, x1, y1);
+    x = y1;
+    y = x1 - (a / b) * y1;
+    return mdc;
+}
 int main(){
-    cin>>a>>b;
-    m=mdc(a,b);
-    while
+   ll a,b,x,y,mdc,mud=0;
+   cin>>a>>b;
+   if(a < b){//bora inverter?
+       ll tmp = a;
+       a = b;
+       b = tmp;
+       mud = 1;
+   }
+   mdc = lol(a, b, x, y);
+   if(mud){//
+       cout<<y<<" "<<x;//coeficientes 
+   }else{
+       cout<<x<<" "<<y;
+   }
+   return 0;
 }
